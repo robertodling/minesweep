b8dace4ed2f27532f7ce3797299ef9b061f88dcc
Robert Ödling <robert.odling@gmail.com>
2014-12-07 20:17:45 +0000

    development environment configured

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 minesweep
 =========
+
+A minesweep clone built to learn [React](http://facebook.github.io/react/index.html) and [TypeScript](http://www.typescriptlang.org/)
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var eventStream = require('event-stream');
+var react = require('gulp-react');
+
+gulp.task('typescript', function () {
+	var tsResult = gulp.src('src/ts**/*.ts')
+		.pipe(ts({
+			declarationFiles: true,
+			noExternalResolve: true
+		}));
+
+	return eventStream.merge(
+		tsResult.dts.pipe(gulp.dest('build/definitions')),
+		tsResult.js.pipe(gulp.dest('build/js'))
+	);
+});
+
+gulp.task('react', function () {
+	return gulp.src('src/**/*.jsx')
+		.pipe(react())
+		.pipe(gulp.dest('build/js'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('src/ts/**/*.ts', ['typescript']);
+	gulp.watch('src/jsx/**/*.jsx', ['react']);
+
+});
+
+
+gulp.task('default', ['typescript', 'react']);
