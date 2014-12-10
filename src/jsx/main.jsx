e20425ffe372270ec0f889b7faf254e0a3fe2ab5
Robert Ödling <robert.odling@gmail.com>
2014-12-10 22:01:41 +0100

    fully working version

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,3 +1,9 @@
+var width = 18;
+var height = 18;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function createCell(row, column){
 	return {
 		row:row,
@@ -5,26 +11,104 @@ function createCell(row, column){
 	}
 }
 
+var alive = true;
+
+
 var gameBoard = {
 	init: function(rows, cols){
+		this.sizeRow = rows;
+		this.sizeCols = cols;
 		this.rows = [];
+		var self = this;
 		for(var r = 0; r < rows; r++){
 			var row = [];
 			 
 			for(var c = 0; c < cols; c++){
-				row.push(createCell(r, c));	
+				var cell = createCell(r, c);
+				
+				row.push(cell);
 			}	
 			
 			this.rows.push(row);
+		}
+		var nrBombs = 100;
+		
+		for(var i = 0; i<nrBombs; i++){
+			console.log(i);
+			var row = getRandomInt(0,rows);
+			var column = getRandomInt(0,cols);
+			this.getCell(row, column).bomb = true;
+		}
+		
+		for(var r = 0; r < rows; r++){
+			 
+			for(var c = 0; c < cols; c++){
+				var cell = self.getCell(r, c);
+				if(cell.bomb){
+					continue;
+				}
+				var number = 0;
+				[-1+r,r,1+r].forEach(function(r2){
+					[-1+c,c,1+c].forEach(function(c2){
+						
+						if(!(c===c2 &&r === r2) && r2>=0 && r2 < self.sizeRow && c2>=0 && c2 < self.sizeCols){
+							var cell2 = self.getCell(r2,c2);
+							if(cell2.bomb){
+								number++;
+							}
+						}
+						
+					});
+				});
+				cell.number = number;
+			}	
 		}
 	},
 	getCell : function(row, column){
 		return this.rows[row][column];
 	},
-	check: function(row, column){
-		var cell = this.getCell(row, column);
-		cell.marked = !cell.marked;
+	flag : function(row, column){
+		var cell = this.getCell(row,column);
+		cell.flag = !cell.flag;
 	},
+	check: function(row, column){
+		var withinBounds = row>=0 && row < this.sizeRow && column>=0 && column < this.sizeCols;
+		if(!withinBounds){
+			return;
+		}
+		
+		var cell = this.getCell(row, column);
+		
+		if(cell.bomb){
+			alive = false;
+			return;
+		}
+		
+	
+	
+		if(cell.flag){
+			return;
+		}
+		if(cell.clicked || cell.number>0){
+			cell.clicked = true;
+			return;
+		}else{
+			cell.clicked = true;
+			
+			this.check(row-1, column-1);
+			this.check(row-1, column);
+			this.check(row-1, column+1);
+			
+			this.check(row, column-1);
+			this.check(row, column+1);
+			
+			this.check(row+1, column-1);
+			this.check(row+1, column);
+			this.check(row+1, column+1);
+		}
+		
+	},
+	
 	
 	// representation of game state
 	toJSON: function (){
@@ -41,10 +125,8 @@ var gameBoard = {
 	}
 }
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
 
+var cx = React.addons.classSet;
 var board = Object.create(gameBoard);
 board.init(30,30);
 
@@ -52,31 +134,52 @@ board.init(30,30);
 function render(){
 	var json = board.toJSON();
 
-var game = React.render(<GameBoard gameBoard={json}/>,  document.getElementById('main'));
-
+	var game = React.render(<GameBoard gameBoard={json}/>,  document.getElementById('main'));
+	React.render(<Status/>,  document.getElementById('status'));
 }
 
 var Cell = React.createClass({
-	handleClick: function(){
+	handleClick: function(e){
+		
+		e.preventDefault();
 		var cell = this.props.cell;
-		board.check(cell.row, cell.column);
+		if(e.button){
+			board.flag(cell.row, cell.column);
+			
+		}else{
+			board.check(cell.row, cell.column);
+		}
 		render();
 	},
 	render: function(){
+		
 		var cell = this.props.cell;
 		var divStyle = {
-			top: cell.row*15,
-			left: cell.column*15,
+			top: cell.row*width,
+			left: cell.column*height,
+			color: cell.number>=3?"#AA0000":(cell.number==2?"#00AA00":"#666666"),
 		}
-		var cx = React.addons.classSet;
+	
 		var classes = cx({
     		'cell': true,
-    		'clicked': cell.marked
-  		});
+    		'clicked': cell.clicked,
+    		'bomb': cell.bomb,
+    		'flag': cell.flag,
+			'fa':cell.flag || cell.bomb,
+			'fa-flag':cell.flag,
+			'fa-bomb':cell.bomb && !alive,
+			'number':cell.number>0 && cell.clicked
 			
-		return <div className={classes} style={divStyle} onClick={this.handleClick}></div>;
+  		});
+		
+			
+		return <div className={classes} style={divStyle} onClick={this.handleClick} onContextMenu={this.handleClick}>
+			{cell.clicked && cell.number>0?cell.number:''}
+			
+		</div>;
 	}
 });
+
 
 var GameBoard = React.createClass({
 	
@@ -87,16 +190,29 @@ var GameBoard = React.createClass({
     	};
 		var divStyle = {
 			position: 'relative',
-			
 			display: 'block',
 			float: 'left',
-			width: 30*15,
-			heigt: 30*15,
+			width: 30*width,
+			heigt: 30*height,
 		}
 		
-		return <div style={divStyle}>{
+		var classes = cx({
+    		'dead': !alive
+  		});
+			
+		
+		return <div style={divStyle} className={classes}>{
 			this.props.gameBoard.cells.map(createItem)
 		};</div>
+	}
+	
+});
+
+var Status = React.createClass({
+	
+  	render: function() {
+	
+		return <div>{alive?'alive':'dead'}</div>
 	}
 	
 });
